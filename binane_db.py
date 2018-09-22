278c946260d26004fdd2ac2b09521df9cdbbba1b
BrentLewis <blewis1991@live.com>
2018-09-21 18:49:20 -0700

    Update binane_db.py

--- binane_db.py
+++ binane_db.py
@@ -147,6 +147,245 @@ def h_l_v_loop(a = 0):
     return print('inserted h_l_v')
 h_l_v_loop(a = 0)
 
+###These are all a repeat of the same finction
+#I'll build a decorator, or class not sure which is best yet, later for now it functions
+def _5m_():
+
+    for a in range(len(Indicators['rsi'])):
+        with db.session.no_autoflush:
+            s = symbol(symbol_col=tickers[symbol])
+            
+            _5m = _5m(stoch_k = Indicators['stoch_k'][a],
+                stoch_d= Indicators['stoch_d'][a],
+                stochrsi= Indicators['stochrsi'][a],
+                rsi=Indicators['rsi'][a],
+                cci= Indicators['cci'][a],
+                ema= Indicators['ema'][a],
+                mfi= Indicators['mfi'][a],
+                psar= Indicators['psar'][a],
+                bbands_upper= Indicators['bbands_upper'][a],
+                bbands_middle= Indicators['bbands_middle'][a],
+                bbands_lower= Indicators['bbands_lower'][a],
+                macd= Indicators['macd'][a],
+                macd_histogram= Indicators['macd_histogram'][a],
+                macd_signal= Indicators['macd_signal'][a])
+
+            s._5m.append(_5m)
+            db.session.add(s)
+            db.session.add(_5m)
+    return print('5m table inserted into')
+
+
+def _10m_():
+
+    for a in range(len(Indicators['rsi'])):
+        with db.session.no_autoflush:
+            s = symbol(symbol_col=tickers[symbol])
+                        
+            _10m = _10m(stoch_k = Indicators['stoch_k'][a],
+                stoch_d= Indicators['stoch_d'][a],
+                stochrsi= Indicators['stochrsi'][a],
+                rsi=Indicators['rsi'][a],
+                cci= Indicators['cci'][a],
+                ema= Indicators['ema'][a],
+                mfi= Indicators['mfi'][a],
+                psar= Indicators['psar'][a],
+                bbands_upper= Indicators['bbands_upper'][a],
+                bbands_middle= Indicators['bbands_middle'][a],
+                bbands_lower= Indicators['bbands_lower'][a],
+                macd= Indicators['macd'][a],
+                macd_histogram= Indicators['macd_histogram'][a],
+                macd_signal= Indicators['macd_signal'][a])
+
+            s._10m.append(_10m)
+            db.session.add(s)
+            db.session.add(_10m)
+    return print('10m table inserted into')
+
+
+def _15m_():
+
+    for a in range(len(Indicators['rsi'])):
+        with db.session.no_autoflush:
+            s = symbol(symbol_col=tickers[symbol])
+                       
+            _15m = _15m(stoch_k = Indicators['stoch_k'][a],
+                stoch_d= Indicators['stoch_d'][a],
+                stochrsi= Indicators['stochrsi'][a],
+                rsi=Indicators['rsi'][a],
+                cci= Indicators['cci'][a],
+                ema= Indicators['ema'][a],
+                mfi= Indicators['mfi'][a],
+                psar= Indicators['psar'][a],
+                bbands_upper= Indicators['bbands_upper'][a],
+                bbands_middle= Indicators['bbands_middle'][a],
+                bbands_lower= Indicators['bbands_lower'][a],
+                macd= Indicators['macd'][a],
+                macd_histogram= Indicators['macd_histogram'][a],
+                macd_signal= Indicators['macd_signal'][a])
+
+            s._15m.append(_15m)
+            db.session.add(s)
+            db.session.add(_15m)
+    return print('15m table inserted into')
+
+
+def _30m_():
+
+    for a in range(len(Indicators['rsi'])):
+        with db.session.no_autoflush:
+            s = symbol(symbol_col=tickers[symbol])
+                         
+            _30m = _30m(stoch_k = Indicators['stoch_k'][a],
+                stoch_d= Indicators['stoch_d'][a],
+                stochrsi= Indicators['stochrsi'][a],
+                rsi=Indicators['rsi'][a],
+                cci= Indicators['cci'][a],
+                ema= Indicators['ema'][a],
+                mfi= Indicators['mfi'][a],
+                psar= Indicators['psar'][a],
+                bbands_upper= Indicators['bbands_upper'][a],
+                bbands_middle= Indicators['bbands_middle'][a],
+                bbands_lower= Indicators['bbands_lower'][a],
+                macd= Indicators['macd'][a],
+                macd_histogram= Indicators['macd_histogram'][a],
+                macd_signal= Indicators['macd_signal'][a])
+
+            s._30m.append(_30m)
+            db.session.add(s)
+            db.session.add(_30m)
+    return print('30m table inserted into')
+      
+def _1h_():
+
+    for a in range(len(Indicators['rsi'])):
+        with db.session.no_autoflush:
+            s = symbol(symbol_col=tickers[symbol])
+                
+            _1h = _1h(stoch_k = Indicators['stoch_k'][a],
+                stoch_d= Indicators['stoch_d'][a],
+                stochrsi= Indicators['stochrsi'][a],
+                rsi=Indicators['rsi'][a],
+                cci= Indicators['cci'][a],
+                ema= Indicators['ema'][a],
+                mfi= Indicators['mfi'][a],
+                psar= Indicators['psar'][a],
+                bbands_upper= Indicators['bbands_upper'][a],
+                bbands_middle= Indicators['bbands_middle'][a],
+                bbands_lower= Indicators['bbands_lower'][a],
+                macd= Indicators['macd'][a],
+                macd_histogram= Indicators['macd_histogram'][a],
+                macd_signal= Indicators['macd_signal'][a])
+
+            s._1h.append(_1h)
+            db.session.add(s)
+            db.session.add(_1h)
+    return print('1h table inserted into')
+
+def _2h_():
+
+    for a in range(len(Indicators['rsi'])):
+        with db.session.no_autoflush:
+            s = symbol(symbol_col=tickers[symbol])
+                         
+            _2h = _2h(stoch_k = Indicators['stoch_k'][a],
+                stoch_d= Indicators['stoch_d'][a],
+                stochrsi= Indicators['stochrsi'][a],
+                rsi=Indicators['rsi'][a],
+                cci= Indicators['cci'][a],
+                ema= Indicators['ema'][a],
+                mfi= Indicators['mfi'][a],
+                psar= Indicators['psar'][a],
+                bbands_upper= Indicators['bbands_upper'][a],
+                bbands_middle= Indicators['bbands_middle'][a],
+                bbands_lower= Indicators['bbands_lower'][a],
+                macd= Indicators['macd'][a],
+                macd_histogram= Indicators['macd_histogram'][a],
+                macd_signal= Indicators['macd_signal'][a])
+
+            s._2h.append(_2h)
+            db.session.add(s)
+            db.session.add(_2h)
+    return print('2h table inserted into')
+
+def _4h_():
+
+    for a in range(len(Indicators['rsi'])):
+        with db.session.no_autoflush:
+            s = symbol(symbol_col=tickers[symbol])
+                  
+            _4h = _4h(stoch_k = Indicators['stoch_k'][a],
+                stoch_d= Indicators['stoch_d'][a],
+                stochrsi= Indicators['stochrsi'][a],
+                rsi=Indicators['rsi'][a],
+                cci= Indicators['cci'][a],
+                ema= Indicators['ema'][a],
+                mfi= Indicators['mfi'][a],
+                psar= Indicators['psar'][a],
+                bbands_upper= Indicators['bbands_upper'][a],
+                bbands_middle= Indicators['bbands_middle'][a],
+                bbands_lower= Indicators['bbands_lower'][a],
+                macd= Indicators['macd'][a],
+                macd_histogram= Indicators['macd_histogram'][a],
+                macd_signal= Indicators['macd_signal'][a])
+
+            s._4h.append(_4h)
+            db.session.add(s)
+            db.session.add(_4h)
+    return print('1h table inserted into')
+
+def _6h_():
+
+    for a in range(len(Indicators['rsi'])):
+        with db.session.no_autoflush:
+            s = symbol(symbol_col=tickers[symbol])
+             
+            _6h = _6h(stoch_k = Indicators['stoch_k'][a],
+                stoch_d= Indicators['stoch_d'][a],
+                stochrsi= Indicators['stochrsi'][a],
+                rsi=Indicators['rsi'][a],
+                cci= Indicators['cci'][a],
+                ema= Indicators['ema'][a],
+                mfi= Indicators['mfi'][a],
+                psar= Indicators['psar'][a],
+                bbands_upper= Indicators['bbands_upper'][a],
+                bbands_middle= Indicators['bbands_middle'][a],
+                bbands_lower= Indicators['bbands_lower'][a],
+                macd= Indicators['macd'][a],
+                macd_histogram= Indicators['macd_histogram'][a],
+                macd_signal= Indicators['macd_signal'][a])
+
+            s._6h.append(_6h)
+            db.session.add(s)
+            db.session.add(_6h)
+    return print('1h table inserted into')
+
+def _12h_():
+    
+    for a in range(len(Indicators['rsi'])):
+        with db.session.no_autoflush:
+            s = symbol(symbol_col=tickers[symbol])
+                     
+            _12h = _12h(stoch_k = Indicators['stoch_k'][a],
+                stoch_d= Indicators['stoch_d'][a],
+                stochrsi= Indicators['stochrsi'][a],
+                rsi=Indicators['rsi'][a],
+                cci= Indicators['cci'][a],
+                ema= Indicators['ema'][a],
+                mfi= Indicators['mfi'][a],
+                psar= Indicators['psar'][a],
+                bbands_upper= Indicators['bbands_upper'][a],
+                bbands_middle= Indicators['bbands_middle'][a],
+                bbands_lower= Indicators['bbands_lower'][a],
+                macd= Indicators['macd'][a],
+                macd_histogram= Indicators['macd_histogram'][a],
+                macd_signal= Indicators['macd_signal'][a])
+
+            s._12h.append(_12h)
+            db.session.add(s)
+            db.session.add(_12h)
+    return print('1h table inserted into')
+
 
 db.create_all()
 db.session.commit()
