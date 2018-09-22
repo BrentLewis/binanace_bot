b8998a00abc0814c100885f79cc6e7f71df99b0f
BrentLewis <blewis1991@live.com>
2018-09-21 18:42:05 -0700

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,66 +1,86 @@
-import talib as tab
+import tulipy as ti
+import numpy as np
 import pandas as pd
 
-klines_1 = np.append(klines[a][i], np.array([('first', 10, 11)], dtype= float64))
-#Are the numbers coming out right?
-def indicators():
-    for i in range(len(klines)):
-        # 2 = High, 3 = Low, 4 = Close, 5 = Volume
-        Indicators = {
-            'SMA': pd.DataFrame(tab.SMA(klines[i][df][4], timeperiod=30) for df in range(len(klines[i]))),
-            'ROC': pd.DataFrame(tab.ROC(klines[i][df][4], timeperiod=10) for df in range(len(klines[i]))),
-            'ROCP': pd.DataFrame(tab.ROCP(klines[i][df][4], timeperiod= 10) for df in range(len(klines[i]))),
-            'ROCR': pd.DataFrame(tab.ROCR(klines[i][df][4], timeperiod=10) for df in range(len(klines[i]))),
-            'RSI': pd.DataFrame(tab.RSI(klines[i][df][4], timeperiod=14) for df in range(len(klines[i]))),
-            'BB': pd.DataFrame(tab.BBANDS(klines[i][df][4],timeperiod= 5, nbdevup=2, nbdevdn=2,matype=0) for df in range(len(klines[i]))),
-            'EMA': pd.DataFrame(tab.EMA(klines[i][df][4], timeperiod=30) for df in range(len(klines[i]))),
-            'MA': pd.DataFrame(tab.MA(klines[i][df][4],timeperiod=30,matype=0) for df in range(len(klines[i]))),
-            'WMA': pd.DataFrame(tab.WMA(klines[i][df][4], timeperiod=30) for df in range(len(klines[i]))),
-            'KAMA': pd.DataFrame(tab.KAMA(klines[i][df][4],timeperiod=30) for df in range(len(klines[i]))),
-            'MACD': pd.DataFrame(tab.MACD(klines[i][df][4], fastperiod=12, slowperiod=26, signalperiod=9) for df in range(len(klines[i]))),
-            'STOCHRSI': pd.DataFrame(tab.STOCHRSI(klines[i][df][4], timeperiod=14, fastk_period=5, fastd_period=3, fastd_matype=0) for df in range(len(klines[i]))),
-            'SAR': pd.DataFrame(tab.SAR(klines[i][df][2], klines[i][df][3],acceleration=0, maximum=0) for df in range(len(klines[i]))),
-            'ATR': pd.DataFrame(tab.ATR(klines[i][df][2], klines[i][df][3], klines[i][df][4], timeperiod=14) for df in range(len(klines[i]))),
-            'NATR': pd.DataFrame(tab.NATR(klines[i][df][2], klines[i][df][3],klines[i][df][4],timeperiod=14) for df in range(len(klines[i]))),
-            'CCI':pd.DataFrame(tab.CCI(klines[i][df][2], klines[i][df][3], klines[i][df][4], timeperiod=14) for df in range(len(klines[i]))),
-            'W%R':pd.DataFrame(tab.WILLR(klines[i][df][2], klines[i][df][3], klines[i][df][4], timeperiod=14) for df in range(len(klines[i]))),
-            'MFI':pd.DataFrame(tab.MFI(klines[i][df][2], klines[i][df][3], klines[i][df][4], klines[i][df][5], timeperiod=14) for df in range(len(klines[i]))),
-            'AD':pd.DataFrame(tab.AD(klines[i][df][2], klines[i][df][3], klines[i][df][4], klines[i][df][5]) for df in range(len(klines[i]))),
-            'DOSC':pd.DataFrame(tab.ADOSC(klines[i][df][2], klines[i][df][3], klines[i][df][4], klines[i][df][5], fastperiod=3, slowperiod=10) for df in range(len(klines[i])))
-                         }
-    return Indicators
-Indicators = indicators(klines)
+# a recursive funciton that computes market indicators and pushes to the appropriate table 
+# it perfroms the operation on one symbol at a  time. see symbol = 0 in arguments
+def indicators(symbol=0,time_frame='5m', time = _5m):
+    # 2 = High, 3 = Low, 4 = Close, 5 = Volume
+    #collects the needed market data into one list to push to the indicators
+    close = np.array([time[symbol][df][4] for df in range(len(time[symbol]))])
+    close = close.astype(np.float64)
+    high = np.array([time[symbol][df][2] for df in range(len(time[symbol]))])
+    high = high.astype(np.float64)
+    low = np.array([time[symbol][df][3] for df in range(len(time[symbol]))])
+    low = low.astype(np.float64)
+    #!!!This needs to be changed. Each volume of the base time must be indexed up to the slice
+    volume = np.array([time[symbol][df][5] for df in range(len(time[symbol]))])
+    volume = volume.astype(np.float64)
+    #these are the indicators currently being used, and recored
+    Indicators ={'stochrsi' : ti.stochrsi(close, 5),
+                 'rsi' : ti.rsi(close, 5),
+                 #indicators that need to be doublechecked
+                 'mfi' : ti.mfi(high, low, close, volume, 5),
+                 'psar' : ti.psar(high, low, .2, 2),
+                 'cci' : ti.cci(high, low, close, 5),
+                 'ema' : ti.ema(close, 5)
+                 }
+#this one is good
+    stoch_k, stoch_d = ti.stoch(high, low, close, 5, 3, 3)
 
-#Make recursive??
-def Indicators(a=0):
-    for i in range(len(klines[a])):
-        Indicators= {
-                'SMA': pd.DataFrame(tab.SMA(df['Close'], timeperiod=30) for df in klines[i]),
-                'ROC': pd.DataFrame(tab.ROC(df['Close'], timeperiod=10) for df in klines[i]),
-                'ROCP': pd.DataFrame(tab.ROCP(df['Close'], timeperiod= 10) for df in klines[i]),
-                'ROCR': pd.DataFrame(tab.ROCR(df['Close'], timeperiod=10) for df in klines[i]),
-                'RSI': pd.DataFrame(tab.RSI(df['Close'], timeperiod=14) for df in klines[i]),
-                'BB': pd.DataFrame(tab.BBANDS(df['Close'],timeperiod= 5, nbdevup=2, nbdevdn=2,matype=0) for df in klines[i]),
-                'EMA': pd.DataFrame(tab.EMA(df['Close'], timeperiod=30) for df in klines[i]),
-                'MA': pd.DataFrame(tab.MA(df['Close'],timeperiod=30,matype=0) for df in klines[i]),
-                'WMA': pd.DataFrame(tab.WMA(df['Close'], timeperiod=30) for df in klines[i]),
-                'KAMA': pd.DataFrame(tab.KAMA(df['Close'],timeperiod=30) for df in klines[i]),
-                'MACD': pd.DataFrame(tab.MACD(df['Close'], fastperiod=12, slowperiod=26, signalperiod=9) for df in klines[i]),
-                'STOCHRSI': pd.DataFrame(tab.STOCHRSI(df['Close'], timeperiod=14, fastk_period=5, fastd_period=3, fastd_matype=0) for df in klines[i]),
-                'SAR': pd.DataFrame(tab.SAR(df['High'], df['Low'],acceleration=0, maximum=0) for df in klines[i]),
-                'ATR': pd.DataFrame(tab.ATR(df['High'], df['Low'], df['Close'], timeperiod=14) for df in klines[i]),
-                'NATR': pd.DataFrame(tab.NATR(df['High'], df['Low'],df['Close'],timeperiod=14) for df in klines[i]),
-                'CCI':pd.DataFrame(tab.CCI(df['High'], df['Low'], df['Close'], timeperiod=14) for df in klines[i]),
-                'W%R':pd.DataFrame(tab.WILLR(df['High'], df['Low'], df['Close'], timeperiod=14) for df in klines[i]),
-                'MFI':pd.DataFrame(tab.MFI(df['High'], df['Low'], df['Close'], df['Volume'], timeperiod=14) for df in klines[i]),
-                'AD':pd.DataFrame(tab.AD(df['High'], df['Low'], df['Close'], df['Volume']) for df in klines[i]),
-                'DOSC':pd.DataFrame(tab.ADOSC(df['High'], df['Low'], df['Close'], df['Volume'], fastperiod=3, slowperiod=10) for df in klines[i])
-                         }
+    # check on this, to see if it functions properly
+    bbands_lower, bbands_middle, bbands_upper = ti.bbands(close, 5, 2)
+    macd, macd_signal, macd_histogram = ti.macd(close, 12, 26, 9)
+
+    Indicators['stoch_k'] = stoch_k
+    Indicators['stoch_d'] = stoch_d
+
+    Indicators['macd'] = macd
+    Indicators['macd_histogram']= macd_histogram
+    Indicators['macd_signal']= macd_signal
+
+    Indicators['bbands_upper']= bbands_upper
+    Indicators['bbands_middle']= bbands_middle
+    Indicators['bbands_lower']= bbands_lower
     
-    a = a + 1
-    if a < len(klines):
-        t_loop(a=a)
-    return print('inserted time')
+    #below changes the length of each array to match the longest one, for a pandas df
+    #np.nan for the tail of the shorter ones
+    b=[]
+    for i in Indicators:
+        b.append(len(Indicators[i]))
+    b= max(b)
 
+    for i in Indicators:
+        if len(Indicators[i]) < b:
+            a= b - len(Indicators[i]) 
+            Indicators[i] = np.append(Indicators[i], [np.nan]*a)
+    
+    Indicators = pd.DataFrame(Indicators)
 
-Indicators(a=0)
+#Below calls the function that pushes all the indicators to the time frames table
+    if time_frame == '5m':
+        _5m_()
+    elif time_frame == '10m':
+        _10m_()
+    elif time_frame == '15m':
+        _15m_()
+    elif time_frame == '30m':
+        _30m_()
+    elif time_frame == '1h':
+        _1h_()
+    elif time_frame == '2h':
+        _2h_()
+    elif time_frame == '4h':
+        _4h_()
+    elif time_frame == '6h':
+        _6h_()
+    elif time_frame == '12h':
+        _12h_()
+
+    symbol = symbol + 1
+    if symbol < len(klines):
+        indicators(symbol=symbol,time_frame = time_frame, time=time)
+    else:
+#returns indicators for attatchment to live trading
+        return Indicators
+
